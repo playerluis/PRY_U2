dc24b17c2d0c531c545dfaffd825debbde23b1dc
Brandxxxn <bjbermello@espe.edu.ec>
2023-07-20 22:48:23 -0500

    Paciente

---
+++ P_U3/Controllers/PacienteController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P_U3.Context;
+using P_U3.Models;
+
+namespace P_U3.Controllers;
+
+[Authorize]
+public class PacienteController : Controller
+{
+    private readonly BddCitasMedicasContext _context;
+
+    public PacienteController(BddCitasMedicasContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Pacientes
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Paciente.ToListAsync());
+    }
+
+    // GET: Pacientes/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var paciente = await _context.Paciente
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (paciente == null)
+        {
+            return NotFound();
+        }
+
+        return View(paciente);
+    }
+
+    // GET: Pacientes/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Pacientes/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Nombre,Apellido,FechaDeNacimiento,Direccion,FechaDeRegistro")] Paciente paciente)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(paciente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(paciente);
+    }
+
+    // GET: Pacientes/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var paciente = await _context.Paciente.FindAsync(id);
+        if (paciente == null)
+        {
+            return NotFound();
+        }
+        return View(paciente);
+    }
+
+    // POST: Pacientes/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Apellido,FechaDeNacimiento,Direccion,FechaDeRegistro")] Paciente paciente)
+    {
+        if (id != paciente.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(paciente);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PacienteExists(paciente.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(paciente);
+    }
+
+    // GET: Pacientes/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var paciente = await _context.Paciente
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (paciente == null)
+        {
+            return NotFound();
+        }
+
+        return View(paciente);
+    }
+
+    // POST: Pacientes/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var paciente = await _context.Paciente.FindAsync(id);
+        if (paciente != null)
+        {
+            _context.Paciente.Remove(paciente);
+        }
+            
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool PacienteExists(int id)
+    {
+        return _context.Paciente.Any(e => e.Id == id);
+    }
+}
---
+++ P_U3/Models/Paciente.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using P_U3.Utils;
+
+namespace P_U3.Models;
+
+public class Paciente : IModeable
+{
+    public int Id { get; set; }
+
+    [Required] 
+    [StringLength(255)] 
+    [DisplayName("Nombre(s)")]
+    public string Nombre { get; set; }
+
+    [Required]
+    [StringLength(255)] 
+    [DisplayName("Apellido(s)")]
+    public string Apellido { get; set; }
+
+    [Required]
+    [DisplayName("Fecha de Nacimiento")]
+    [DataType(DataType.Date)]
+    public DateOnly FechaDeNacimiento { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    [DisplayName("Dirección")]
+    public string Direccion { get; set; }
+
+}
